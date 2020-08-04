0a306152dd4867f008bc0898af4fc77f1892056a
TASAKAKOKI <67449952+TASAKAKOKI@users.noreply.github.com>
2020-08-04 16:13:00 +0900

    Add files via upload

---
+++ 003_Number-Guesser_js/resources/js/script.js
@@ -0,0 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => Math.floor(Math.random() * 10);
+// const dif = (input, target) => {
+//     if (input >= target) {
+//         return input - target;
+//     }
+//     else {
+//         return target - input;
+//     }
+// };
+// above function can be replaced by 'Math.abs()'
+const compareGuesses = (user, pc, target) => {
+    if (Math.abs(user - target) > Math.abs(pc - target)) {
+        return false;
+    }
+    else if (Math.abs(user - target) < Math.abs(pc - target)) {
+        return true;
+    }
+    else {
+        return true;
+    }
+};
+
+const updateScore = winner => {
+    if (winner == 'human') {
+        humanScore += 1;
+    }
+    else if (winner == 'computer') {
+        computerScore += 1;
+    }
+};
+
+const advanceRound = () => currentRoundNumber += 1;
